f032584fbf709cc42a8807d5dc1d92b13338664b
soferdani <soferdani@gmail.com>
2021-11-16 15:13:17 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<Router>
+      <div className='App'>
+        <NavBar/>
+      </div>
+		</Router>
+	);
 }
-
-export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+
+
+export default function NavBar() {
+    return (
+        <div className='nav'>
+            this is a component
+        </div>
+    );
+}
