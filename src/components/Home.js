685012fadb6652c7e0e99fc06f3b8a753b008da2
soferdani <soferdani@gmail.com>
2021-11-28 15:59:00 +0200

    dotenv added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,10 @@
+
+let nasaKey = process.env.REACT_APP_NASA_API_KEY;
+
 export default function Home() {
+    console.log(nasaKey);
+    let reqUrl = `https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`;
+
     return (
         <div className='home'>
             this is a component
